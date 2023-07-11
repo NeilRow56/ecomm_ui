3e7815a07e66b6d972c029c0258b27a8c0eb553e
Neil Rowland <neilrowland56@gmail.com>
2023-07-11 14:00:29 +0100

    Added billboard to Home Page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,19 @@
-export default function HomePage() {
-  return <main className="">Hello Store</main>;
-}
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("a2165cc2-57db-4283-953d-37a246727130");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
